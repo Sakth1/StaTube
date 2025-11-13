fa81fb680b4b234c1d08acdec1768d8bb6052867
Sakthi Murugan C <sakthimurugankgm@gmail.com>
2025-11-13 20:41:28 +0530

    small changes in appstate logic

--- utils/AppState.py
+++ utils/AppState.py
@@ -1,58 +1,73 @@
 from PySide6.QtCore import QObject, Signal
+from typing import Optional, Any
+
+from Data.DatabaseManager import DatabaseManager
+
 
 class AppState(QObject):
     """
     Global application state singleton.
     Stores shared data and proxy instance.
     """
-    channel_name_changed = Signal(str)
-    channel_id_changed = Signal(str)
-    channel_url_changed = Signal(str)
+    channel_info_changed = Signal(dict)
+    video_list_changed = Signal(dict)
+    video_list_appended = Signal(dict)
     
     def __init__(self):
         super().__init__()
-        self._channel_name = None
-        self._channel_id = None
-        self._channel_url = None
+        self._channel_info = None
+        self._video_list = None
         self._db = None
+
+
+    @property
+    def channel_info(self):
+        return self._channel_info
+    
+    @channel_info.setter
+    def channel_info(self, value: Any) -> dict:
+        if value is None:
+            return
+
+        if isinstance(value, dict):
+            self._channel_info = value
+            self.channel_info_changed.emit(self._channel_info)
+
+        elif isinstance(value, tuple) and len(value) == 2:
+            key, value = value
+            if self._channel_info[key] != value:
+                self._channel_info[key] = value
+                self.channel_info_changed.emit(self._channel_info)
+
+
+    @property
+    def video_list(self) -> dict:
+        return self._video_list
+    
+    @video_list.setter
+    def video_list(self, value: Any):
+        if value is None:
+            return
         
-    @property
-    def channel_name(self):
-        return self._channel_name
-    
-    @channel_name.setter
-    def channel_name(self, value):
-        if self._channel_name != value:
-            self._channel_name = value
-            self.channel_name_changed.emit(value)
-    
-    @property
-    def channel_id(self):
-        return self._channel_id
-    
-    @channel_id.setter
-    def channel_id(self, value):
-        if self._channel_id != value:
-            self._channel_id = value
-            self.channel_id_changed.emit(value)
+        if isinstance(value, dict):
+            self._video_list = value
+            self.video_list_changed.emit(self._video_list)
+
+        if isinstance(value, tuple) and len(value) == 2:
+            key, value = value
+            if self._video_list[key] != value:
+                self._video_list[key] = value
+                self.video_list_appended.emit(self._video_list)
+
     
     @property
-    def channel_url(self):
-        return self._channel_url
-    
-    @channel_url.setter
-    def channel_url(self, value):
-        if self._channel_url != value:
-            self._channel_url = value
-            self.channel_url_changed.emit(value)
-    
-    @property
-    def db(self):
+    def db(self) -> DatabaseManager:
         return self._db
     
     @db.setter
-    def db(self, value):
+    def db(self, value: DatabaseManager):
         self._db = value
+
 
 # Global singleton instance
 app_state = AppState()
