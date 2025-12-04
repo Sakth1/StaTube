bb93bbb8e08817df743e40cd4e90ffff8c855446
Sakthi Murugan C <sakthimurugankgm@gmail.com>
2025-12-04 16:48:09 +0530

    logger improvement

--- utils/logger.py
+++ utils/logger.py
@@ -1,33 +1,21 @@
 import logging
 import os
-import time
+from datetime import datetime
 import platform
 from pathlib import Path
-import argparse
 
 def get_documents_dir():
     system = platform.system()
     return Path(os.path.expanduser("~/Documents"))
 
-def is_debug_mode():
-    # Option A: --debug
-    parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument("--debug", action="store_true")
-    args, _ = parser.parse_known_args()
-
-    # Option B: ENV variable
-    env_flag = os.environ.get("STATUBE_DEBUG", "0") == "1"
-
-    return args.debug or env_flag
-
 def setup_logger():
-    timestamp_ms = int(time.time() * 1000)
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     documents_dir = get_documents_dir()
 
     log_dir = documents_dir / "StaTube"
     log_dir.mkdir(parents=True, exist_ok=True)
 
-    file_path = log_dir / f"statube_log_{timestamp_ms}.log"
+    file_path = log_dir / f"statube_log_{timestamp}.log"
 
     logger = logging.getLogger("StaTube")
     logger.setLevel(logging.DEBUG)
@@ -43,12 +31,10 @@ def setup_logger():
     fh.setFormatter(formatter)
     logger.addHandler(fh)
 
-    # Console handler only if debug mode
-    if is_debug_mode():
-        ch = logging.StreamHandler()
-        ch.setLevel(logging.DEBUG)
-        ch.setFormatter(formatter)
-        logger.addHandler(ch)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
 
     logger.propagate = False
     return logger, file_path
